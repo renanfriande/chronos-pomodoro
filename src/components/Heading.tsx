41cbbe7ea7d5e0e18f4b9113fd94e692a7fa3ffa
Renan Friande <renanfriandee@gmail.com>
2025-06-05 19:27:30 -0300

    Variáveis CSS e configurações globais do body no CSS

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,6 +1,5 @@
 import styles from './Heading.module.css'
 
 export function Heading (props) {
-  console.log(props)
   return <h1 className={`${styles.heading} ${styles.cyan}`}>{props.children}</h1>
 }
