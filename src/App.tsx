ca526144b4dbb13a4e3aa23d27e3720ba8aabf23
Renan Friande <renanfriandee@gmail.com>
2025-06-05 19:43:52 -0300

    Configurações de tipagem, import do lucide-react e module do Heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { Heading } from "./components/Heading";
+
 import "./styles/global.css";
 import "./styles/theme.css";
+import { TimerIcon } from "lucide-react";
 
 export function App() {
   return (
     <>
-      <Heading>Ola mundo!</Heading>
+      <Heading>
+        Ola mundo!
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos voluptates
         sint numquam est vel in reprehenderit, laborum obcaecati recusandae
