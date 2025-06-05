7ed61d3d74fc7e196627870f0ef04c82987210ab
Renan Friande <renanfriandee@gmail.com>
2025-06-05 20:12:22 -0300

    Tipagem, componente de Container e module Container CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
+import { Container } from "./components/Container";
 import { Heading } from "./components/Heading";
-
 import "./styles/global.css";
 import "./styles/theme.css";
-import { TimerIcon } from "lucide-react";
 
 export function App() {
   return (
     <>
-      <Heading>
-        Ola mundo!
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos voluptates
-        sint numquam est vel in reprehenderit, laborum obcaecati recusandae
-        magni voluptatem similique asperiores deleniti ut possimus alias atque
-        quaerat consequuntur.
-      </p>
+      <Container>
+        <Heading>Chronos</Heading>
+      </Container>
+
+      <Container>
+        <section>MENU</section>
+      </Container>
+
+      <Container>
+        <section>FORM</section>
+      </Container>
+
+      <Container>
+        <section>FOOTER</section>
+      </Container>
     </>
   );
 }
