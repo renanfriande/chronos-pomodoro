b07f03e680076f67ea745982338c9fec302b67ae
Renan Friande <renanfriandee@gmail.com>
2025-06-08 20:43:00 -0300

    Corrigindo o App pra adaptar o contexto

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
-import { useState } from "react";
 import { Home } from "./pages/Home";
-
-import type { TaskStateModel } from "./models/TaskStateModel";
+import { TaskContextProvider } from "./contexts/TaskContext";
 
 import "./styles/global.css";
 import "./styles/theme.css";
-import { TaskContextProvider } from "./contexts/TaskContext";
-
-const initialState: TaskStateModel = {
-  tasks: [],
-  secondsRemaining: 0,
-  formattedSecondsRemaining: "00:00",
-  activeTask: null,
-  currentCycle: 0,
-  config: {
-    workTime: 25,
-    shortBreakTime: 5,
-    longBreakTime: 15,
-  },
-};
 
 export function App() {
-  const [state, setState] = useState(initialState);
-
   return (
     <TaskContextProvider>
       <Home />
