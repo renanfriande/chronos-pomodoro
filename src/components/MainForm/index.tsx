1d54e2ddb1f967b21008c3a2c22d7db06447c891
Renan Friande <renanfriandee@gmail.com>
2025-06-09 20:35:55 -0300

    Computando os ciclos

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -5,10 +5,15 @@ import { DefaultInput } from "../DefaultInput";
 import { useRef } from "react";
 import type { TaskModel } from "../../models/TaskModel";
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "../../utils/getNextCycle";
+import { getNextCycleType } from "../../utils/getNextCycleType";
 
 export function MainForm() {
-  const { setState } = useTaskContext();
+  const { state, setState } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
+
+  const nextCycle = getNextCycle(state.currentCycle)
+  const nextCycleType = getNextCycleType(nextCycle)
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -29,7 +34,7 @@ export function MainForm() {
       startDate: Date.now(),
       completeDate: null,
       interruptDate: null,
-      type: "workTime",
+      type: nextCycleType,
     };
 
     const secondsRemaining = newTask.duration * 60;
@@ -39,7 +44,7 @@ export function MainForm() {
         ...prevState,
         config: { ...prevState.config },
         activeTask: newTask,
-        currentCycle: 1, //Conferir
+        currentCycle: nextCycle,
         secondsRemaining, //Conferir
         formattedSecondsRemaining: "00:00", //Conferir
         tasks: [...prevState.tasks, newTask],
