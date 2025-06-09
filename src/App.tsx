4cc685b7aa60a7577572c2841d34257ac00c3065
Renan Friande <renanfriandee@gmail.com>
2025-06-09 19:00:37 -0300

    Melhoria na organização do TaskContext, fragmentando o index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Home } from "./pages/Home";
-import { TaskContextProvider } from "./contexts/TaskContext";
+import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
 
 import "./styles/global.css";
 import "./styles/theme.css";
