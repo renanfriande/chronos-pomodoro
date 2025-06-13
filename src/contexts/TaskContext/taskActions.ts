b2fc3308da1ef4b6fb1b74d448dbd2fec553286a
Renan Friande <renanfriandee@gmail.com>
2025-06-13 19:35:37 -0300

    Começando a migração para Reducer

---
+++ src/contexts/TaskContext/taskActions.ts
@@ -0,0 +1,16 @@
+import type { TaskModel } from "../../models/TaskModel";
+
+export enum TaskActionsTypes {
+  START_TASK = "START_TASK",
+  INTERRUPT_TASK = "INTERRUPT_TASK",
+}
+
+export type TaskActionModel =
+  | {
+      type: TaskActionsTypes.START_TASK;
+      payload: TaskModel;
+    }
+  | {
+      type: TaskActionsTypes.INTERRUPT_TASK;
+      payload: TaskModel;
+    };
---
+++ src/contexts/TaskContext/taskReducer.ts
@@ -0,0 +1,18 @@
+import type { TaskStateModel } from "../../models/TaskStateModel";
+import { TaskActionsTypes, type TaskActionModel } from "./taskActions";
+
+export function taskReducer(
+  state: TaskStateModel,
+  action: TaskActionModel
+): TaskStateModel {
+  switch (action.type) {
+    case TaskActionsTypes.START_TASK: {
+      return state;
+    }
+    case TaskActionsTypes.INTERRUPT_TASK: {
+      return state;
+    }
+  }
+
+  return state;
+}
